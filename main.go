1fa12e6364165ad87f595994a9ec98c2e60c8e9a
Theo Pack <tf.pack@gmail.com>
2018-02-06 19:21:19 +0100

    Add raw endpoint

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/rand"
+	"fmt"
 	"html/template"
 	"net/http"
 	"os"
@@ -9,6 +11,25 @@ import (
 type Page struct {
 	Title string
 	Body  []byte
+}
+
+func raw(w http.ResponseWriter, r *http.Request) {
+	value, exists := os.LookupEnv("RAW_CONTENT")
+	if !exists {
+		value = pseudo_uuid()
+	}
+	fmt.Fprintf(w, value)
+}
+
+func pseudo_uuid() string {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return ""
+	}
+
+	return fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +42,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	http.HandleFunc("/raw", raw)
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
